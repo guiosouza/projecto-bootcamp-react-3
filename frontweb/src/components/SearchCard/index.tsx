f74350615c1e4793fe17b57fa67b733358f0d7d8
guiosouza <guilhermedeoliveiradesouza@gmail.com>
2022-10-17 10:10:17 -0300

    Response CardSearch in 576px

--- frontweb/src/components/SearchCard/index.tsx
+++ frontweb/src/components/SearchCard/index.tsx
@@ -5,6 +5,7 @@ const SearchCard = () => {
     return (
         <div className="base-card search-card">
             <div>
+                <label htmlFor="btn btn-secondary"></label>
                 <input type="text" placeholder='Digite sua busca'/><button className="btn btn-secondary">Buscar</button>
             </div>
         </div>
--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -1,26 +1,30 @@
 import ProductCard from 'components/ProductCard';
 import SearchCard from 'components/SearchCard';
+import './styles.css';
 
 const Catalog = () => {
   return (
-    <div className="container my-4">
+    <div className="container">
       <div className="row">
         <div className="col-sm-12 col-lg-12 col-xl-12">
           <SearchCard/>
         </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
+        <div className="col-sm-6 col-lg-4 col-xl-4">
           <ProductCard />
         </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
+        <div className="col-sm-6 col-lg-4 col-xl-4">
           <ProductCard />
         </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
+        <div className="col-sm-6 col-lg-4 col-xl-4">
           <ProductCard />
         </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
+        <div className="col-sm-6 col-lg-4 col-xl-4">
           <ProductCard />
         </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
+        <div className="col-sm-6 col-lg-4 col-xl-4">
+          <ProductCard />
+        </div>
+        <div className="col-sm-6 col-lg-4 col-xl-4">
           <ProductCard />
         </div>
       </div>
